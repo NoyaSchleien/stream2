dcdfe574ddeb7eb2e10720e4743b4966b8bfc0ca
NoyaSchleien <noyaschleien@gmail.com>
2017-12-17 10:18:17 +0200

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  checked: boolean = false;
+  stream: any;
+  videoSource:SourceBuffer;
+  errorMessage: string;
+  mediaSource:MediaSource;
+
+  @ViewChild("myVideo") myVideo: HTMLVideoElement;
+// @ViewChild("mediaSource") mediaSource:MediaSource;
+  constructor(private _appService: AppService) { }
+  ngOnInit(){
+this.mediaSource=new MediaSource();
+this.videoSource = this.mediaSource.addSourceBuffer('application/octet-stream');
+  }
+  onChange(e) {
+    if (e.target.checked) {
+      this.checked = true;
+      this._appService.getStream()
+      .subscribe(stream => {
+        this.stream = stream;
+        console.log(this.stream);
+        try {
+        // this.videoSource = this.myVideo['nativeElement'].addSourceBuffer('Uint8Array');
+          this.videoSource.appendBuffer(new Uint8Array(this.stream));
+          this.myVideo['nativeElement'].play();
+      } catch (e) {
+          console.log('Exception while appending', e);
+      }
+      },
+      error => this.errorMessage = <any>error
+    );
+  } else {
+    this.checked = false;
+    this.myVideo['nativeElement'].pause();
+    }
+}
 }
