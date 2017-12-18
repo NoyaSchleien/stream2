50c5d32e9aa9fe7cb18cccd5ce48243f3293cee2
NoyaSchleien <noyaschleien@gmail.com>
2017-12-18 09:23:05 +0200

    problem: connection never opens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,50 +9,66 @@ import { AppService } from './app.service';
 export class AppComponent {
   checked: boolean = false;
   stream: any;
-  videoSource:SourceBuffer;
+  sourceBuffer: SourceBuffer;
   errorMessage: string;
-  mediaSource:MediaSource;
+  mediaSource: MediaSource;
 
   @ViewChild("myVideo") myVideo: HTMLVideoElement;
-// @ViewChild("mediaSource") mediaSource:MediaSource;
   constructor(private _appService: AppService) { }
-  ngOnInit(){
-this.mediaSource=new MediaSource();
-this.mediaSource.addEventListener('sourceopen', function (e) {
-  try {
-      this.videoSource = this.mediaSource.addSourceBuffer('application/octet-stream');
-  }
-      catch (e) {
-        console.log('Exception calling addSourceBuffer for video', e);
-        return;
-    }
-}, false);
-// this.videoSource=this.myVideo['nativeElement'].addSourceBuffer;
-// // this.videoSource = this.mediaSource.addSourceBuffer('application/octet-stream');
-}
-onChange(e) {
-  if (e.target.checked) {
-    this.checked = true;
-    this._appService.getStream()
-    .subscribe(stream => {
-      this.stream = new Blob([stream.blob],{type:'application/octet-stream'});
-      console.log(this.stream);
-      try{
-      this.videoSource.appendBuffer(new Uint8Array(this.stream));
-        // this.videoSource= this.mediaSource.addSourceBuffer(new Uint8Array());       
-          // this.videoSource=this.stream;
-        // this.videoSource = this.myVideo['nativeElement'].addSourceBuffer('Uint8Array');
-          // this.videoSource.appendBuffer(new Uint8Array(this.stream));
-          this.myVideo['nativeElement'].play();
-      } catch (e) {
-          console.log('Exception while appending', e);
-      }
-      },
-      error => this.errorMessage = <any>error
-    );
-  } else {
+ 
+  onChange(e) {
+    if (e.target.checked) {
+      this.checked = true;
+
+      this.mediaSource = new MediaSource();
+      console.log("media source = " + this.mediaSource);
+      console.log("media source ready state = " + this.mediaSource.readyState);
+      // this.myVideo['nativeElement'].src = URL.createObjectURL(this.mediaSource);
+      this.mediaSource.addEventListener('sourceOpen', this.sourceOpen);
+
+    } else {
     this.checked = false;
     this.myVideo['nativeElement'].pause();
-    }
+  }
 }
+  sourceOpen(){
+    console.log("mediaSource should be open now, ready state = " + this.mediaSource.readyState )
+    console.log("media source after open event = " + this.mediaSource);
+    try {
+      this.sourceBuffer = this.mediaSource.addSourceBuffer('application/octet-stream');
+    }
+    catch (e) {
+      console.log('Exception calling addSourceBuffer for video', e);
+      return;
+    }
+    this.getStream();
+  }
+  getStream(){
+  // console.log("URL = " + this.myVideo['nativeElement'].src)
+
+  this._appService.getStream()
+  .subscribe(stream => {
+    this.stream = new Blob([stream.blob], { type: 'application/octet-stream' });
+    console.log(this.stream);
+    this.sourceBuffer.addEventListener('updateEnd', this.updateEnd);
+    try {
+      this.sourceBuffer.appendBuffer(new Uint8Array(this.stream));
+      // this.sourceBuffer= this.mediaSource.addSourceBuffer(new Uint8Array());       
+      // this.sourceBuffer=this.stream;
+      // this.sourceBuffer = this.myVideo['nativeElement'].addSourceBuffer('Uint8Array');
+      // this.sourceBuffer.appendBuffer(new Uint8Array(this.stream));
+    } catch (e) {
+      console.log('Exception while appending', e);
+    }
+  },
+  error => this.errorMessage = <any>error
+);
+}  
+
+updateEnd(){
+  this.mediaSource.endOfStream();
+  console.log("mediasource state should be ended = " + this.mediaSource.readyState);
+  this.myVideo['nativeElement'].play();
+  }
+
 }
